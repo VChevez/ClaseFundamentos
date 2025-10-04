cdeba891bf832e6c9d0f52a438aada636ee6e9f2
VChevez <alfredo2006chevez@gmail.com>
2025-10-04 06:38:27 +0200

    update

--- Clase Fundamentos/src/Main.java
+++ Clase Fundamentos/src/Main.java
@@ -1,2 +1,15 @@
+// Archivo: Principal.java
 public class Main {
+    public static void main(String[] args) {
+        // Crear un objeto de la clase Persona
+        Persona p1 = new Persona("Carlos", 19,"tomates");
+        Persona p2 = new Persona("Farry", 22,"frituras");
+        Persona p3 = new Persona("Tobar",25,"aguacates");
+        // Usar los métodos de la clase Persona
+        p1.saludar();
+        p2.saludar();
+        p3.saludar();
+
+
+}
 }
