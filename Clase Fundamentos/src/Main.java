345abfd6b1657ff12ec83736ad29ac611d21af10
VChevez <alfredo2006chevez@gmail.com>
2025-10-06 01:08:21 +0200

    update

--- Clase Fundamentos/src/Main.java
+++ Clase Fundamentos/src/Main.java
@@ -3,11 +3,20 @@ public class Main {
 
     public static void main(String[]args) {
         Scanner h = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad a descomponer: ");
         double a = h.nextDouble();
-        double resultado = (double) 1/a;
-        System.out.println(resultado);
-        System.out.println("Estas son las monedas necesarias para llegar a un dólar");
+        double[] b  = {1.0, 0.75, 0.5, 0.25, 0.1, 0.05, 0.01};
+        System.out.println("Descomposición de " + a + ":");
+        double m = 0;
+        System.out.println(" moneda/s de " + a + " = 0" );
+        for (double moneda : b) {
+            int j = (int) Math.floor((a + 1e-9) / moneda);
+            if (j > 0){
+                System.out.println(j + " moneda/s de " + moneda);
+                a -= j * moneda;}
+
+
 
     }
-
-    }
+}
+}
